7c3807fe48cdf5d95e1f5d5e6c9da2f2288d98b9
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-01-29 23:42:58 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".burger").on("click", function () {
+    $(".burger").toggleClass("active");
+    $("#header").toggleClass("active");
+  });
+
+  let prevScrollPos = window.scrollY;
+  const $header = $("#header");
+
+  $(window).on("scroll", function () {
+    const currentScrollPos = window.scrollY;
+    $header.css(
+      "top",
+      prevScrollPos > currentScrollPos || currentScrollPos === 0
+        ? "31px"
+        : "-205px"
+    );
+    $header.removeClass("active");
+    $(".burger").removeClass("active");
+    prevScrollPos = currentScrollPos;
+  });
+});
