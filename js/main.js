aef3bf37866f74c6b23ffbaadd5a6bad0dd33af0
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-01-30 19:13:38 +0600

    laoyt make

--- js/main.js
+++ js/main.js
@@ -19,4 +19,67 @@ $(document).ready(function () {
     $(".burger").removeClass("active");
     prevScrollPos = currentScrollPos;
   });
+
+  $(".tabby-link").on("click", function (e) {
+    e.preventDefault();
+
+    var tabId = $(this).attr("href");
+    $(
+      `.woman_wrp .wrapper-weight > h3, 
+      .woman_wrp .wrapper-cost > h3, 
+      .woman_wrp .wrapper-info > p, 
+      .woman_wrp .wrapper-info > .calculation > h4`
+    ).hide();
+
+    $(tabId).show();
+    $(tabId + "-cost").show();
+    $(tabId + "-comp").show();
+    $(tabId + "-food").show();
+    $(tabId + "-squi").show();
+    $(tabId + "-fat").show();
+    $(tabId + "-carb").show();
+  });
+
+  $(".tabby-link[data-tabby-default]").click();
+
+  $(".tabby-man-link").on("click", function (e) {
+    e.preventDefault();
+
+    var tabId = $(this).attr("href");
+    $(
+      `.man__card .wrapper-weight > h3,
+      .man__card .wrapper-cost > h3,
+      .man__card .wrapper-info > p,
+      .man__card .wrapper-info > .calculation > h4`
+    ).hide();
+
+    $(tabId).show();
+    $(tabId + "-cost").show();
+    $(tabId + "-comp").show();
+    $(tabId + "-food").show();
+    $(tabId + "-squi").show();
+    $(tabId + "-fat").show();
+    $(tabId + "-carb").show();
+  });
+
+  $(".tabby-man-link[data-tabby-default]").click();
+
+  $('input[type="radio"][name="activity"]').on("change", function () {
+    var type = $(this).data("type");
+    var text = "";
+
+    switch (type) {
+      case "easy":
+        text = "Текст для низкого уровня активности";
+        break;
+      case "medium":
+        text = "Текст для среднего уровня активности";
+        break;
+      case "hard":
+        text = "Текст для высокого уровня активности";
+        break;
+    }
+
+    $(".desc-goal").text(text);
+  });
 });
